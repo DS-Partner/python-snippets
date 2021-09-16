72eba7c5209bddc5886670e01fea3bed4759f36d
Rodrigo Arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2021-09-16 18:05:33 -0500

    randint

---
+++ numpy/randint.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Lowest number to possibly generate
+low = 5
+# One avobe th largest number to possibly generate
+high = 11
+# Dimensions of the array
+size = (3, 4)
+
+np.random.randint(low, high, size)
