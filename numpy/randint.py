3ed8b912d525c7049a6c569682c59145a5f099e8
Rodrigo Arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2021-09-16 18:11:49 -0500

    typo

--- numpy/randint.py
+++ numpy/randint.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Lowest number to possibly generate
 low = 5
-# One avobe th largest number to possibly generate
+# One avobe the largest number to possibly generate
 high = 11
 # Dimensions of the array
 size = (3, 4)
